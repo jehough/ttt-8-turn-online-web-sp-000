c8f3489ed2e506f12b20ed4203be6dd27c50db85
Jim Hough <teachlife@outlook.com>
2019-04-12 21:40:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,16 @@ def display_board (board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move? (board, index)
+  move = nil
+  if index > 8
+    move = false
+  elsif !(board[index] == " " || board[index] == "" ||board[index] == nil)
+    move = false
+  else
+    move = true
+  end
+  move
+end
+
+def move
